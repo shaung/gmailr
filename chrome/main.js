a0911616f2cfa89543d52cdbe60140c64ad52cf1
shaung <shaun.geng@gmail.com>
2013-10-18 20:29:32 +0900

    pon

--- chrome/main.js
+++ chrome/main.js
@@ -14,6 +14,51 @@ Gmailr.init(function(G) {
         G.$('#gmailr #status').html(msg);
     };
 
+    var generateRandomString = function(s){
+        var f = function(){
+            return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+        }
+        return '__hoge__shaung.org__' + f();
+    };
+
+    var replaceSelectedText = function(replace){
+        var tempStr = generateRandomString();
+        if (window.getSelection) {
+            var sel = window.getSelection();
+            if (sel.rangeCount) {
+                var range = sel.getRangeAt(0);
+                var selstr = range.toString();
+                range.deleteContents();
+                range.insertNode(document.createTextNode(tempStr));
+            }
+        } else if (document.selection && document.selection.createRange) {
+            var range = document.selection.createRange();
+            var selstr = range.toString();
+            range.text = tempStr;
+        }
+        if (selstr) {
+            $('.editable').html($('.editable').html().replace(replace(selstr), tempStr));
+        }
+    };
+
+    $('#cancel-text-button').live('click', function(){
+        replaceSelectedText(function(s){
+            return '<span style="color: red; text-decoration: line-through;">' + s + '</span>';
+        });
+    });
+
+    var makeCancelButton = function(){
+        if (!$('#cancel-text-button').length) {
+            $('div[command=+underline]').each(function(){
+                $('<button id="cancel-text-button">取消<button>').insertAfter($(this));
+            });
+        }
+    };
+
+    G.observe('viewChanged', function(){
+        makeCancelButton();
+    });
+
     G.observe('archive', function(num) {
         status('You archived ' + num + ' emails.');
     });
